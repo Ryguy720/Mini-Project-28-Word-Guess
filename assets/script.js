c9a49c816686a74209a77a26a418d420920b7b37
unknown <mccullochryan636@gmail.com>
2021-09-15 21:30:38 -0600

    initial comit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+let startbtn = document.querySelector(".start-button");
+let resetbtn = document.querySelector(".reset-button");
+let cardwordGuess = document.querySelector(".card-word-guess")
+
+startbtn.addEventListener("click" , function(){
+
+
+
+}) 
